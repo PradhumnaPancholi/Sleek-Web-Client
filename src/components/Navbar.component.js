2660f09fd3fdb256afba7771a98d22b974d13119
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2019-10-01 18:01:13 -0400

    created basic navbar and react-router setup

---
+++ src/components/Navbar.component.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import MenuIcon from '@material-ui/icons/Menu';
+
+
+// eslint-disable-next-line react/prefer-stateless-function
+class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton edge="start" color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6">
+              Sleek
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
