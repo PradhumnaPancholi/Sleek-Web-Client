ed1d28afb5547338e64e4e4cbd8ca4aaa2f52bf6
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2019-10-18 23:48:17 -0400

    added grid and minor naing improv for avatar item componenet

--- src/components/AvatarsList.component.tsx
+++ src/components/AvatarsList.component.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import { GridList, GridListTile } from '@material-ui/core';
 
 export default function AvatarsList() {
     
     const array = ['avatar01', 'avatar02', 'avatar03', 'avatar04', 'avatar05', 'avatar06', 'avatar07', 'avatar08', 'avatar09']
-
-    const avatarItems = array.map(item => {
-        return item
-    })
-
     return(
-        <p>{avatarItems}</p>
+        <div>
+            <GridList cellHeight={160} cols={3}>
+                {array.map(item => (
+                    <GridListTile>
+                        <p>{item}</p>
+                    </GridListTile>
+                ))}
+            </GridList>
+        </div>
     )
 }
