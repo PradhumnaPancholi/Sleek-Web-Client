27cec68c5297eccb6440c591ce6b3e7ad2ca3a82
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2019-12-06 14:25:51 -0500

    started to clean up the code with hooks, Need to implement useEffects

--- src/components/LoginForm.component.tsx
+++ src/components/LoginForm.component.tsx
@@ -1,5 +1,5 @@
 import { Button, Card, CardContent, TextField, Typography } from '@material-ui/core'
-import React, { Component } from 'react'
+import React, { FunctionComponent, useState } from 'react'
 import { connect } from 'react-redux'
 
 import { signInUserAction } from '../actions/user.actions'
@@ -7,86 +7,79 @@ import { signInUserAction } from '../actions/user.actions'
 //interface for state//
 interface LoginFormState{
     email: string
-    password: string
+    password: string 
 }
 
 // eslint-disable-next-line react/prefer-stateless-function
-class LoginForm extends Component<{}, Partial<LoginFormState>>{
-
-    constructor(props: any) {
-        super(props)
-        this.state = {
-            email: '',
-            password: ''
-        }
-    }
-
-    signIn = async () => {
+// const LoginForm: FunctionComponent<Partial<LoginFormState>> = () => {
+const LoginForm: FunctionComponent = () => {
+    
+    const [credentials , setCredentials] = useState<LoginFormState>({
+        email: '',
+        password: ''
+    })
+    
+    const signIn = async () => {
         console.log("this is from sign in")
         // this.props.signInUserAction()
     }
 
-    handleInputChanges = (e: any) => {
+    const handleInputChanges = (e: any) => {
         const {name, value} = e.target
-        this.setState({ 
-            [name]: value
-        })
+        // setState.name = value
     }
-    
-    render() {
-        const { email, password } = this.state
-        return(
-            <div className= 'login-form'>
-                <Card>
-                    <CardContent className='login-content'>
-                        <Typography variant='h5' component='h2'>
-                            Log Into Your Account
-                        </Typography>
-                        <div>
-                            <TextField
-                                id='outlined-email-input'
-                                label='Email'
-                                type='email'
-                                name='email'
-                                value={email}
-                                onChange={this.handleInputChanges}
-                                autoComplete='email'
-                                margin='normal'
-                                variant='outlined'
-                                InputProps={{
-                                    className:'login-input-props'
-                                }}
-                            />
-                        </div>
-                        <div>
-                            <TextField
-                                id='outlined-password-input'
-                                label='Password'
-                                type='password'
-                                name='password'
-                                value={password}
-                                onChange={this.handleInputChanges}
-                                autoComplete='current-password'
-                                margin='normal'
-                                variant='outlined'
-                                InputProps={{
-                                    className:'login-input-props'
-                                }}
-                            />
-                        </div>
-                        <div>
-                            <Button variant='contained'>
-                                Log In
-                            </Button>
-                        </div>
-                        <Typography variant='h6' component='h5'>
-                            Don't have an account yet? <a href='/signup'>Sign Up</a>
-                        </Typography>
-                    </CardContent>
-                </Card>
-            </div>
-        )
-    }
+
+    return(
+        <div className= 'login-form'>
+            <Card>
+                <CardContent className='login-content'>
+                    <Typography variant='h5' component='h2'>
+                        Log Into Your Account
+                    </Typography>
+                    <div>
+                        <TextField
+                            id='outlined-email-input'
+                            label='Email'
+                            type='email'
+                            name='email'
+                            value={credentials.email}
+                            onChange={this.handleInputChanges}
+                            autoComplete='email'
+                            margin='normal'
+                            variant='outlined'
+                            InputProps={{
+                                className:'login-input-props'
+                            }}
+                        />
+                    </div>
+                    <div>
+                        <TextField
+                            id='outlined-password-input'
+                            label='Password'
+                            type='password'
+                            name='password'
+                            value={credentials.password}
+                            onChange={this.handleInputChanges}
+                            autoComplete='current-password'
+                            margin='normal'
+                            variant='outlined'
+                            InputProps={{
+                                className:'login-input-props'
+                            }}
+                        />
+                    </div>
+                    <div>
+                        <Button variant='contained'>
+                            Log In
+                        </Button>
+                    </div>
+                    <Typography variant='h6' component='h5'>
+                        Don't have an account yet? <a href='/signup'>Sign Up</a>
+                    </Typography>
+                </CardContent>
+            </Card>
+        </div>
+    )
 }
 
 const mapStateToProps = (state: any ) => ({
