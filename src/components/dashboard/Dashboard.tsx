9680f2962f3f442ad50b6bac0427ba69ac74da53
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2020-02-26 18:11:25 -0500

    basic setup for message board

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -1,10 +1,14 @@
 import React, { FunctionComponent } from 'react'
 
 import ChannelDrawer from '../channels/ChannelDrawer.component'
+import { MessageBoard } from '../messages/MessageBoard'
 
 const Dashboard: FunctionComponent = () => {
     return(
+        <div>
         <ChannelDrawer />
+        <MessageBoard/>
+        </div>
     )
 }
 
