af626966b84bb40a0ee70893794f4b72b7360440
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2020-02-26 18:51:33 -0500

    added component and basic layout for messageitem

--- src/components/messages/MessageBoard.tsx
+++ src/components/messages/MessageBoard.tsx
@@ -1,9 +1,14 @@
 import React, { FunctionComponent } from 'react'
 
+import { MessageItem } from './MessageItem'
+
 export const MessageBoard: FunctionComponent = () => {
     return(
         <div className='message-board'>
-            
+            <MessageItem/>
+            <MessageItem/>
+            <MessageItem />
+            <MessageItem />
         </div>
     )
 }
